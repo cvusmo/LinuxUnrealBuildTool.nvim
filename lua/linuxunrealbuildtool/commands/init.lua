70b65bb6a6c1d251fe0f21558468b2551c7555fd
cvusmo <blacksheepcosmo@gmail.com>
2024-06-09 12:16:29 -0500

    fixing dev_test

--- lua/linuxunrealbuildtool/commands/init.lua
+++ lua/linuxunrealbuildtool/commands/init.lua
@@ -1,4 +1,4 @@
--- linuxunrealbuildtool.commands init
+
 if 1 ~= vim.fn.has "nvim-0.10.0" then
   vim.api.nvim_err_writeln "LinuxUnrealBuildTool.nvim requires at least nvim-0.10.0"
   return
@@ -16,30 +16,14 @@ vim.api.nvim_create_user_command("LUBT", function(opts)
   end
   local command = args[1]
   print("Received command: " .. (command or "nil"))
-  table.remove(args, 1)
-  local sub_opts = { args = table.concat(args, " ") }
-  print("Sub options: " .. vim.inspect(sub_opts))
-
+  
   if command == "DEVTEST" then
     print("Running DEVTEST")
     require("linuxunrealbuildtool.commands.dev_test").devtest()
-  elseif command == "Clean" then
-    require("linuxunrealbuildtool.commands.clean").clean(sub_opts)
-  elseif command == "Build" then
-    require("linuxunrealbuildtool.commands.build").build(sub_opts)
-  elseif command == "Rebuild" then
-    require("linuxunrealbuildtool.commands.rebuild").rebuild(sub_opts)
-  elseif command == "Run" then
-    require("linuxunrealbuildtool.commands.run_project").run_project(sub_opts)
-  elseif command == "CreateProject" then
-    require("linuxunrealbuildtool.commands.create_project").create_project(sub_opts)
-  elseif command == "InstallPlugins" then
-    require("linuxunrealbuildtool.commands.install_plugins").install_plugins(sub_opts)
-  elseif command == "Help" then
-    require("linuxunrealbuildtool.commands.help").help(sub_opts)
-  else print("LUBT command does not exist. Please use :LUBT Help for list of commands")
+  else
+    print("LUBT command does not exist. Please use :LUBT Help for list of commands")
   end
-end, { nargs = "*" } )
+end, { nargs = "*" })
 
 local function setup()
   print("Configuring :LUBT commands")
@@ -48,3 +32,4 @@ end
 return {
   setup = setup
 }
+
