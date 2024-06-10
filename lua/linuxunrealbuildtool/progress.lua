8b8add1cb906623f721c78d398513edd975ac09d
cvusmo <blacksheepcosmo@gmail.com>
2024-06-10 05:55:07 -0500

    Progress.lua fixed function init and function update.

--- lua/linuxunrealbuildtool/progress.lua
+++ lua/linuxunrealbuildtool/progress.lua
@@ -3,14 +3,14 @@ local log = require("linuxunrealbuildtool.log")
 local M = {}
 
 --f(progressinitialize)
-function M.progress_init(total_steps, project_path, log_suffix)
+function M.init(total_steps, project_path, log_suffix)
   M.total_steps = total_steps or 0
   M.current_step = 0
   log.setup(log_suffix, project_path)
 end
 
 --f(progressupdate)
-function M.progress_update(current_step)
+function M.update(current_step)
   M.current_step = current_step
   local percentage = math.floor((M.current_step * 100) / M.total_steps)
   log.log_message("Progress: " .. percentage .. "% completed")
