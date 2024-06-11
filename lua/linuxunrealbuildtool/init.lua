232099c9ea264d06631f84daa59f347da28f5abd
cvusmo <blacksheepcosmo@gmail.com>
2024-06-11 08:17:05 -0500

    Test 1: implementing notify.nvim

--- lua/linuxunrealbuildtool/init.lua
+++ lua/linuxunrealbuildtool/init.lua
@@ -1,15 +1,19 @@
 local M = {}
 
+local notify = require("notify")
+vim.notify = notify
 local config = require("linuxunrealbuildtool.config")
 local path = require("linuxunrealbuildtool.path")
 local log = require("linuxunrealbuildtool.log")
 local commands = require("linuxunrealbuildtool.commands")
 
+-- f(init_paths)
 local function init_paths()
   local paths = path.init_paths()
   return paths
 end
 
+-- f(setup)
 local function setup(user_config)
   if user_config then
     config.set_paths(user_config)
@@ -17,7 +21,7 @@ local function setup(user_config)
   init_paths()
   log.setup("log_suffix", config.project_root)
   commands.setup()
-  print("Configuring LinuxUnrealBuildTool")
+  vim.notify("Configuring LinuxUnrealBuildTool", vim.log.levels.INFO)
 end
 
 M.setup = setup
