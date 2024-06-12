ed29b4b00cf241281ce8698fc323d11d71021e81
cvusmo <blacksheepcosmo@gmail.com>
2024-06-12 06:41:02 -0500

    Test 5: run command

--- lua/linuxunrealbuildtool/commands/run.lua
+++ lua/linuxunrealbuildtool/commands/run.lua
@@ -1,6 +1,6 @@
 local log = require("linuxunrealbuildtool.log")
 local path = require("linuxunrealbuildtool.path")
-local config = require("linuxunrealbuildtool.config")
+local progress = require("linuxunrealbuildtool.progress")
 
 local M = {}
 
@@ -23,28 +23,39 @@ function M.run(args)
     return
   end
 
-  local project_path = config.get_paths().project_root .. "/" .. project_name
+  local project_path = paths.current_project .. "/" .. project_name
   local log_suffix = "RunProject"
   log.setup(log_suffix, project_path)
   local log_file = log.get_log_file()
 
   log.log_trashcollector()
 
-  local target_file = project_path .. "/Binaries/Linux/" .. project_name .. "Editor.target"
-  if vim.fn.filereadable(target_file) == 0 then
-    log_message("Target file " .. target_file .. " does not exist. Please build the project first.")
+  local total_steps = 3
+  local current_step = 0
+
+  progress.init(total_steps, project_path, log_suffix)
+
+  local project_editor = project_path .. "/Binaries/Linux/" .. project_name .. "Editor"
+  if vim.fn.filereadable(project_editor) == 0 then
+    log_message("Project editor " .. project_editor .. " does not exist. Please build the project first.")
     return
   end
+  current_step = current_step + 1
+  progress.update(current_step)
 
   local uproject_file = project_path .. "/" .. project_name .. ".uproject"
   if vim.fn.filereadable(uproject_file) == 0 then
     log_message("Project file " .. uproject_file .. " does not exist.")
     return
   end
+  current_step = current_step + 1
+  progress.update(current_step)
 
   log_message("Starting Project " .. project_name .. "...")
-  local command = paths.unreal_editor .. " \"" .. uproject_file .. "\" | tee -a " .. log_file
-  local result = os.execute(command)
+  local run_command = paths.unreal_editor .. " \"" .. uproject_file .. "\""
+  local result = log.execute_command(run_command .. " | tee -a " .. log_file)
+  current_step = current_step + 1
+  progress.update(current_step)
 
   if result == 0 then
     log_message("Project " .. project_name .. " started.")
